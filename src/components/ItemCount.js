bb56a60f9b652f7b2d4552cf26080b1523c4932b
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2021-08-25 01:24:23 -0300

    fifth-challenge

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,6 +1,6 @@
 const ItemCount = ({ counter, onSubtractHandler, onAddHandler }) => {
     return (
-        <div>
+        <div className="div-button">
             <button className="button-counter" onClick={onSubtractHandler}>-</button>
             <h2>{counter}</h2>
             <button className="button-counter" onClick={onAddHandler}>+</button>
