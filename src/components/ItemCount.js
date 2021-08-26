da9971135de7e616729e6b679454af2780946032
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2021-08-26 20:04:37 -0300

    sixth-challenge

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,32 +1,37 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const ItemCount = ({ initial, stock }) => {
-    const [counter, setCounter] = useState(initial)
+  const [counter, setCounter] = useState(initial);
 
-    const onAddHandler = () => {
-        if(counter === stock) return;
-        setCounter(counter + 1)
-    }
-    const onSubtractHandler = () => {
-        if(counter === initial) return;
-        setCounter(counter - 1)
-    }
-    const onAdd = () => {
-        if(counter === 0) return;
-        console.log(`Se agrega al carrito ${counter} unidades`)
-    }
+  const onAddHandler = () => {
+    if (counter === stock) return;
+    setCounter(counter + 1);
+  };
+  const onSubtractHandler = () => {
+    if (counter === initial) return;
+    setCounter(counter - 1);
+  };
+  const onAdd = () => {
+    if (counter === 0) return;
+    console.log(`Se agrega al carrito ${counter} unidades`);
+  };
 
-    return (
-        <div className="div-buttons">
-            <div className="div-counter">
-                <button className="button-counter" onClick={onSubtractHandler}>-</button>
-                <h2>{counter}</h2>
-                <button className="button-counter" onClick={onAddHandler}>+</button>
-            </div>
-            <button className="add-to-cart-button" onClick={onAdd}>Agregar al carrito</button>
+  return (
+    <div className="div-buttons">
+      <div className="div-counter">
+        <button className="button-counter" onClick={onSubtractHandler}>
+          -
+        </button>
+        <h2>{counter}</h2>
+        <button className="button-counter" onClick={onAddHandler}>
+          +
+        </button>
+      </div>
+      <button className="add-to-cart-button" onClick={onAdd}>
+        Agregar al carrito
+      </button>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default ItemCount
+export default ItemCount;
